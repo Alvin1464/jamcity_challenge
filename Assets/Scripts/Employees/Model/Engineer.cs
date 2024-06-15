ec11fd6eab7a2b1558dd9f9ca26dc3ea1b9cd61b
Alvin <alvaro.ainfante@gmail.com>
2024-06-15 20:13:54 -0300

    Engineer base salary tests

--- Assets/Scripts/Employees/Model/Engineer.cs
+++ Assets/Scripts/Employees/Model/Engineer.cs
@@ -1,7 +1,31 @@
+using System;
+using static Employees.Model.Currency;
+
 namespace Employees.Model
 {
     public class Engineer : Employee
     {
+        static readonly Salary juniorBaseSalary = new Salary(1500, DOLLARS);
+        static readonly Salary semiSeniorBaseSalary = new Salary(3000, DOLLARS);
+        static readonly Salary seniorBaseSalary = new Salary(5000, DOLLARS);
         
+        public Engineer(Seniority seniority) : base(seniority, calculateBaseSalary(seniority))
+        {
+        }
+
+        static Salary calculateBaseSalary(Seniority seniority)
+        {
+            switch (seniority)
+            {
+                case Seniority.Junior:
+                    return juniorBaseSalary;
+                case Seniority.Semi_Senior:
+                    return semiSeniorBaseSalary;
+                case Seniority.Senior:
+                    return seniorBaseSalary;
+                default:
+                    return new Salary();
+            }
+        }
     }
 }
--- Assets/Scripts/Tests/Employees/Model/EngineerTests.cs
+++ Assets/Scripts/Tests/Employees/Model/EngineerTests.cs
@@ -1,5 +1,7 @@
 using Employees.Model;
 using NUnit.Framework;
+using static Employees.Model.Currency;
+using static Employees.Model.Seniority;
 using Assert = UnityEngine.Assertions.Assert;
 
 namespace Tests.Employees.Model
@@ -7,9 +9,26 @@ namespace Tests.Employees.Model
     [TestFixture]
     public class EngineerTests
     {
+        Engineer engineer;
+        
         [Test]
         public void EngineersAreEmployees() => 
             Assert.IsTrue(typeof(Engineer).IsSubclassOf(typeof(Employee)));
-        
+
+        [Test]
+        [TestCase(Junior, 1500, DOLLARS)]
+        [TestCase(Semi_Senior, 3000, DOLLARS)]
+        [TestCase(Senior, 5000, DOLLARS)]
+        public void EngineerBaseSalaryDependsOfTheSeniority(Seniority seniority, int amount, Currency currency)
+        {
+            GivenAEngineerWithSeniority(seniority);
+            ThenEngineerSalaryIs(new Salary(amount, currency));
+        }
+
+        void GivenAEngineerWithSeniority(Seniority seniority) => 
+            engineer = new Engineer(seniority);
+
+        void ThenEngineerSalaryIs(Salary salary) => 
+            Assert.AreEqual(engineer.GetSalary(), salary);
     }
 }
