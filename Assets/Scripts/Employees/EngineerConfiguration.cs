66ffb69453f25141fda55e86b8c060bdd82fded8
Alvin <alvaro.ainfante@gmail.com>
2024-06-16 13:18:21 -0300

    refactor: moved engineer salary and raise constants to a configuration class

---
+++ Assets/Scripts/Employees/EngineerConfiguration.cs
@@ -0,0 +1,29 @@
+namespace Employees.Model
+{
+    public static class EngineerConfiguration {
+        static readonly Salary juniorBaseSalary = new(1500, Currency.DOLLARS);
+        static readonly Salary semiSeniorBaseSalary = new(3000, Currency.DOLLARS);
+        static readonly Salary seniorBaseSalary = new(5000, Currency.DOLLARS);
+        static readonly float juniorSalaryIncrementPercentage = 5f;
+        static readonly float semiSeniorSalaryIncrementPercentage = 7f;
+        static readonly float seniorSalaryIncrementPercentage = 10f;
+        
+        public static float GetSalaryIncrementPercentage(Seniority seniority) =>
+            seniority switch
+            {
+                Seniority.Junior => juniorSalaryIncrementPercentage,
+                Seniority.Semi_Senior => semiSeniorSalaryIncrementPercentage,
+                Seniority.Senior => seniorSalaryIncrementPercentage,
+                _ => 0f
+            };
+        
+        public static Salary GetBaseSalary(Seniority seniority) =>
+            seniority switch
+            {
+                Seniority.Junior => juniorBaseSalary,
+                Seniority.Semi_Senior => semiSeniorBaseSalary,
+                Seniority.Senior => seniorBaseSalary,
+                _ => new Salary()
+            };
+    }
+}
--- Assets/Scripts/Employees/Model/Engineer.cs
+++ Assets/Scripts/Employees/Model/Engineer.cs
@@ -1,43 +1,18 @@
 using System;
-using static Employees.Model.Currency;
+using static Employees.Model.EngineerConfiguration;
 
 namespace Employees.Model
 {
     public class Engineer : Employee
     {
-        static readonly Salary juniorBaseSalary = new(1500, DOLLARS);
-        static readonly Salary semiSeniorBaseSalary = new(3000, DOLLARS);
-        static readonly Salary seniorBaseSalary = new(5000, DOLLARS);
-        static readonly float juniorSalaryIncrementPercentage = 5f;
-        static readonly float semiSeniorSalaryIncrementPercentage = 7f;
-        static readonly float seniorSalaryIncrementPercentage = 10f;
-        
         public Engineer(Seniority seniority) : base(seniority, GetBaseSalary(seniority))
         {
         }
 
         public void ApplySalaryIncrement()
         {
-            var newAmount = salary.Amount * GetSalaryIncrementPercentage() * 0.01f + salary.Amount;
+            var newAmount = salary.Amount * GetSalaryIncrementPercentage(seniority) * 0.01f + salary.Amount;
             salary = new Salary(newAmount, salary.SalaryCurrency);
         }
-
-        float GetSalaryIncrementPercentage() =>
-            seniority switch
-            {
-                Seniority.Junior => juniorSalaryIncrementPercentage,
-                Seniority.Semi_Senior => semiSeniorSalaryIncrementPercentage,
-                Seniority.Senior => seniorSalaryIncrementPercentage,
-                _ => 0f
-            };
-
-        static Salary GetBaseSalary(Seniority seniority) =>
-            seniority switch
-            {
-                Seniority.Junior => juniorBaseSalary,
-                Seniority.Semi_Senior => semiSeniorBaseSalary,
-                Seniority.Senior => seniorBaseSalary,
-                _ => new Salary()
-            };
     }
 }
